f5e13bd32dda0a1bb4ae48187963a5936ce91016
lopesdea91 <lopesdea91@gmail.com>
2023-03-30 07:07:43 -0300

    remove unused files

--- src/styles/layout/dsadaspublic.styled.ts
+++
@@ -1,9 +0,0 @@
-import styled from "@emotion/styled";
-
-export const Container = styled.div`
-  min-height: 100vh;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-`
