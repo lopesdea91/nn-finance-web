8a51fb2a46c03197fe2a7d0fc3ebc98f35f94454
lopesdea91 <lopesdea91@gmail.com>
2023-05-02 20:23:37 -0300

    update logic of loading when change page

--- src/layouts/LayoutPrivate/index.tsx
+++ src/layouts/LayoutPrivate/index.tsx
@@ -1,31 +1,19 @@
 import { useEffect } from 'react'
 import { useRouter } from 'next/router'
-import { SystemStore, useAppSelector } from '@/store/hook'
+import { SystemStore } from '@/store/hook'
 import { Container, Header, Main, Menu, Toast } from './components'
 
 export default function LayoutPrivate({ children }: { children: React.ReactNode }) {
-  const { systemState } = useAppSelector((e) => ({
-    systemState: e.system
-  }))
-
   const router = useRouter()
   const systemStore = SystemStore()
 
   useEffect(() => {
-    const handleStart = (url: string) => (url !== router.asPath) && systemStore.loadingPageStart()
-    const handleComplete = (url: string) => (url === router.asPath) && systemStore.loadingPageEnd()
-
-    router.events.on('routeChangeStart', handleStart)
-    router.events.on('routeChangeComplete', handleComplete)
-
-    return () => {
-      router.events.on('routeChangeStart', handleStart)
-      router.events.on('routeChangeComplete', handleComplete)
-    }
-  })
+    router.events.on('routeChangeStart', () => systemStore.loadingPageStart())
+    router.events.on('routeChangeComplete', () => systemStore.loadingPageEnd())
+  }, [])
 
   return (
-    <Container status={systemState.menu}>
+    <Container status={systemStore.state.menu}>
       <Toast />
       <Menu />
       <Header />
